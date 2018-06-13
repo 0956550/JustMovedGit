5e8e3a73bbca842bcf88c1b889b1378147ea36e7
Unknown <0885087@hr.nl>
2018-06-13 12:54:12 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ JustMovedGit/Activities/Items/VeiligheidActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace JustMovedGit.Activities.Items
+{
+    [Activity(Label = "", Theme = "@android:style/Theme.NoTitleBar")]
+    public class VeiligheidActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.PlannenView);
+
+            string id = Intent.GetStringExtra("id");
+            TextView titel = FindViewById<TextView>(Resource.Id.txtReceptTitel);
+            TextView beschrijving = FindViewById<TextView>(Resource.Id.txtReceptBeschrijving);
+            ImageButton favorieten = FindViewById<ImageButton>(Resource.Id.favorietenReceptBtn);
+
+            Models.VeiligheidModel model = new Models.VeiligheidModel();
+            List<Classes.Veiligheid> veiligheid = model.GetSingleData(id);
+            Classes.Veiligheid deveiligheid = veiligheid[0];
+
+            titel.Text = deveiligheid.ongeval;
+            beschrijving.Text = deveiligheid.handeling;
+
+            favorieten.Click += delegate
+            {
+                Console.WriteLine(model.setFavorite(id));
+            };
+        }
+    }
+}
---
+++ JustMovedGit/Models/VeiligheidModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using JustMovedGit.Classes;
+using SQLite;
+
+namespace JustMovedGit.Models
+{
+    class VeiligheidModel
+    {
+        private SQLiteConnection conn;
+
+        public VeiligheidModel()
+        {
+            this.conn = new SQLiteConnection(DbHandler.GetLocalFilePath("JustMovedDb.sqlite"));
+        }
+
+        public List<Veiligheid> GetAllData()
+        {
+            List<Veiligheid> veiligheid = conn.Query<Veiligheid>("SELECT * FROM veiligheid")
+                .ToList();
+            return veiligheid;
+        }
+
+        public List<Veiligheid> GetSearchData(string query)
+        {
+            List<Veiligheid> veiligheid = conn.Query<Veiligheid>("SELECT * FROM veiligheid")
+                .Where(r => r.ongeval.ToLower().Contains(query))
+                .ToList();
+            return veiligheid;
+        }
+
+        public List<Veiligheid> GetSingleData(string id)
+        {
+            List<Veiligheid> veiligheid = conn.Query<Veiligheid>("SELECT * FROM veiligheid")
+                .Where(r => r.id.Equals(id))
+                .ToList();
+            return veiligheid;
+        }
+
+        public Boolean setFavorite(string id)
+        {
+            List<Veiligheid> recepten = conn.Query<Veiligheid>("UPDATE plannen SET favorite = 1 WHERE ID =" + id);
+            if (Int32.Parse(GetSingleData(id)[0].favorite) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
