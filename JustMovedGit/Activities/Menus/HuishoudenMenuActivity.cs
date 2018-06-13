67f578655b757a07d65e0d44d5f37c39bba80a76
Charles <charlottewaldschmit@gmail.com>
2018-06-13 15:34:57 +0200

    doet ie t nu?

--- JustMovedGit/Activities/Menus/HuishoudenMenuActivity.cs
+++ JustMovedGit/Activities/Menus/HuishoudenMenuActivity.cs
@@ -9,17 +9,48 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using JustMovedGit.Adapters;
+using JustMovedGit.Classes;
+using JustMovedGit.Models;
+using JustMovedGit.Activities.Items;
 
 namespace JustMovedGit.Activities
 {
     [Activity(Label = "HuishoudenActivity", Theme = "@android:style/Theme.NoTitleBar")]
     public class HuishoudenMenuActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+            HuishoudenModel model = new HuishoudenModel();
 
-            // Create your application here
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                base.OnCreate(savedInstanceState);
+                SetContentView(Resource.Layout.MenuView);
+                ListView huishoudenMenu = FindViewById<ListView>(Resource.Id.ListView);
+                LinearLayout linearLayout = FindViewById<LinearLayout>(Resource.Id.LinearLayout);
+                List<Huishouden> huishouden = model.GetAllData();
+                HuishoudenAdapter adapter = new HuishoudenAdapter(this, huishouden, Resource.Layout.OtherMenuListview);
+                huishoudenMenu.Adapter = adapter;
+                EditText searchBar = FindViewById<EditText>(Resource.Id.searchBar);
+
+                huishoudenMenu.ItemClick += (s, e) =>
+                {
+                    Intent huishoudenActivity = new Intent(this, typeof(HuishoudenActivity));
+                    huishoudenActivity.PutExtra("id", adapter.GetHuishouden(e.Position).id);
+                    this.StartActivity(huishoudenActivity);
+                };
+
+                searchBar.TextChanged += searchBar_TextChanged;
+
+                void searchBar_TextChanged(object sender, EventArgs e)
+                {
+                    string query = searchBar.Text.ToLower();
+                    List<Huishouden> searchHuishouden = model.GetSearchData(query);
+                    adapter = new HuishoudenAdapter(this, searchHuishouden, Resource.Layout.OtherMenuListview);
+                    huishoudenMenu.Adapter = adapter;
+                };
+            }
         }
+    }
+}
     }
 }
